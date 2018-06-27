ed44c5aa7f90a9dccc64ff0fcdb725b198788f4c
Aniek Gul <aniek.gul@mac-1227-ag.local>
2018-06-27 11:58:25 -0400

    Updated dataflow to model

--- src/main/scala/hack/train/LinearRegressionWithSGDExample.scala
+++ src/main/scala/hack/train/LinearRegressionWithSGDExample.scala
@@ -32,25 +32,47 @@ import org.apache.spark.mllib.regression.LinearRegressionWithSGD
 object LinearRegressionWithSGDExample {
   val log = Logger.getLogger(getClass().getName())
 
+  def convertToSeconds(t: String): Integer = {
+    val split = t.split(":")
+    val seconds = split(0).toInt*3600 + split(1).toInt*60 + split(2).toInt
+    seconds
+  }
+
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("LinearRegressionWithSGDExample")
     val sc = new SparkContext(conf)
 
-    // $example on$
-    // Load and parse the data
-    val data = sc.textFile("/data/mllib/ridge-data/lpsa.data")
-    val parsedData = data.map { line =>
-      val parts = line.split(',')
-      LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(' ').map(_.toDouble)))
-    }.cache()
+    val trips = sc.textFile("/data/ttc/trips.txt")
+      .map(line => {
+        val split = line.split(",")
+        (split(2), split(0))
+      })
+
+    val tripMap = sc.broadcast(trips.collectAsMap)
+
+    val stopTimes = sc.textFile("/data/ttc/stop_times.txt")
+      .map(line => { 
+        val split = line.split(",")
+        (split(0), convertToSeconds(split(1)), split(3), tripMap.value.getOrElse(split(0), "routeIDnotFOund"))
+      })
+      .map{ case(tripId, arrival, stop, route) => LabeledPoint(arrival.toDouble, Vectors.dense(Array(tripId, stop, route).map(_.toDouble)))}
+      .cache()
+
+    // // $example on$
+    // // Load and parse the data
+    // val data = sc.textFile("/data/mllib/ridge-data/lpsa.data")
+    // val parsedData = data.map { line =>
+    //   val parts = line.split(',')
+    //   LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(' ').map(_.toDouble)))
+    // }.cache()
 
     // Building the model
-    val numIterations = 100
-    val stepSize = 0.00000001
-    val model = LinearRegressionWithSGD.train(parsedData, numIterations, stepSize)
+    val numIterations = 10
+    val stepSize = 0.0000000000000001
+    val model = LinearRegressionWithSGD.train(stopTimes, numIterations, stepSize)
 
     // Evaluate model on training examples and compute training error
-    val valuesAndPreds = parsedData.map { point =>
+    val valuesAndPreds = stopTimes.map { point =>
       val prediction = model.predict(point.features)
       (point.label, prediction)
     }
@@ -61,9 +83,11 @@ object LinearRegressionWithSGDExample {
     FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
 
     // Save and load model
-    model.save(sc, "/data/model/tmp/scalaLinearRegressionWithSGDModel")
-    val sameModel = LinearRegressionModel.load(sc, "/data/model/tmp/scalaLinearRegressionWithSGDModel")
-    // $example off$
+    model.save(sc, "/data/model/tmp/testModel")
+
+    valuesAndPreds.saveAsTextFile("/data/model/tmp/predictions")
+    // val sameModel = LinearRegressionModel.load(sc, "/data/model/tmp/scalaLinearRegressionWithSGDModel")
+    // // $example off$
 
     sc.stop()
   }
